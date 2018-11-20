74a624364e5a71a940552476c72b9a0c8f880b64
Glenn Svanberg <signeratsvanberg@gmail.com>
2018-11-20 14:00:11 +0100

    Add project files

---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,75 @@
+<template>
+    <v-container>
+        <v-layout row wrap v-if="loading">
+            <v-flex xs12 class="text-xs-center">
+                <v-progress-circular
+                indeterminate
+                class="primary--text"
+                :width="7"
+                :size="70"
+                v-if="loading"></v-progress-circular>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap v-else>
+            <v-flex xs12>
+                <v-card>
+                    <v-card-title>
+                        <h1 class="primary--text">{{meetup.title}}</h1>
+                        <template v-if="userIsCreator">
+                            <v-spacer></v-spacer>
+                            <app-edit-meetup-details-dialog :meetup="meetup"></app-edit-meetup-details-dialog>
+                        </template>
+                    </v-card-title>
+                       <v-card-media
+                          :src="meetup.imageUrl"
+                          height="400px"
+                          contain
+                        ></v-card-media>
+                        <v-card-text>
+                            <div class="info--text">{{meetup.date | date}} - {{meetup.location}}</div>
+                            <div>
+                                <app-edit-meetup-date-dialog 
+                                :meetup="meetup" 
+                                v-if="userIsAuthenticated"
+                                ></app-edit-meetup-date-dialog>
+                                <app-edit-meetup-time-dialog
+                                :meetup="meetup"
+                                v-if="userIsAuthenticated"
+                                ></app-edit-meetup-time-dialog>
+                                </div>
+                            <div>{{meetup.description}}</div>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <app-meetup-register-dialog v-if="userIsAuthenticated && !userIsCreator" :meetupId="meetup.id"></app-meetup-register-dialog>
+                        </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+<script>
+export default {
+  props: ["id"],
+  computed: {
+    meetup() {
+      return this.$store.getters.loadedMeetup(this.id);
+    },
+    userIsAuthenticated() {
+      return (
+        this.$store.getters.user !== null &&
+        this.$store.getters.user !== undefined
+      );
+    },
+    loading() {
+      return this.$store.getters.loading;
+    },
+    userIsCreator() {
+      if (!this.userIsAuthenticated) {
+        return false;
+      }
+      return this.$store.getters.user.id === this.meetup.creatorId;
+    }
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,58 @@
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "./assets/shared.css";
+import { store } from "./store";
+import DateFilter from "./filters/date";
+import * as firebase from "firebase";
+import AlertCmp from "./components/Shared/Alert.vue";
+import EditMeetupDetailsDialog from "./components/Meetup/Edit/EditMeetupDetailsDialog.vue";
+import EditMeetupDateDialog from "./components/Meetup/Edit/EditMeetupDateDialog.vue";
+import EditMeetupTimeDialog from "./components/Meetup/Edit/EditMeetuptimeDialog.vue";
+import RegisterDialog from "./components/Meetup/Registration/RegisterDialog.vue";
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: "#D32F2F",
+    accent: "#FF5252",
+    secondary: "#BDBDBD",
+    info: "#1E88E5",
+    warning: "#FFA000",
+    error: "#D50000",
+    success: "#66BB6A"
+  }
+});
+Vue.config.productionTip = false;
+
+Vue.filter("date", DateFilter);
+Vue.component("app-alert", AlertCmp);
+Vue.component("app-edit-meetup-details-dialog", EditMeetupDetailsDialog);
+Vue.component("app-edit-meetup-date-dialog", EditMeetupDateDialog);
+Vue.component("app-edit-meetup-time-dialog", EditMeetupTimeDialog);
+Vue.component("app-meetup-register-dialog", RegisterDialog);
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  router,
+  store,
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBVPbj-lQktf_E78SMjqJxqoYJsFOAtanI",
+      authDomain: "event-4adf8.firebaseapp.com",
+      databaseURL: "https://event-4adf8.firebaseio.com",
+      projectId: "event-4adf8",
+      storageBucket: "event-4adf8.appspot.com"
+    });
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$store.dispatch("autoSignIn", user);
+        this.$store.dispatch("fetchUserData");
+      }
+    });
+    this.$store.dispatch("loadMeetups");
+  }
+});
