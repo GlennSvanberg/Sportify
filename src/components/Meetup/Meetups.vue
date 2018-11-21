aa339710bd20f0c04803abdf703642ebb70f6b44
Glenn Svanberg <signeratsvanberg@gmail.com>
2018-11-21 17:06:53 +0100

    Add filter field in Meetups

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,9 +1,18 @@
 <template>
-    <v-container>
-        <v-layout row wrap v-for="meetup in meetups" :key="meetup.id" class="mb-2"> 
-            <v-flex xs12 sm10  md8 offset-sm1 offset-md2>
+    <v-container fluid grid-list-md>
+        <v-layout row wrap>
+            <v-flex xs12 sm12 md2>
+                <v-card class="accent">
+                    <v-card-title>
+                        Hej
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+            <v-flex xs12 md10>
+                <v-layout row wrap > 
+            <v-flex xs12 sm12 md12 lg6 v-for="meetup in meetups" :key="meetup.id" class="mb-2">
                 <v-card class="info" >
-                    <v-container fluid>
+                    <v-container fluid grid-list-md>
                         <v-layout row>
                             <v-flex xs5 sm4 md3>
                                 <v-card-media
@@ -31,6 +40,9 @@
                 </v-card>
             </v-flex>
         </v-layout>
+            </v-flex>
+        </v-layout>
+        
     </v-container>
 </template>
 <script>
